9b8ba853cef60d107568fb1e9efee635bd0625bd
keembogeul <318qhtmf@gmail.com>
2023-10-16 18:27:28 +0900

    bonnie - init

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -13,6 +13,19 @@ export const todoSlice = createSlice({
   name: 'todo',
   initialState: initialValue,
   reducers: {
+    // sample
+    addTodo: state => {
+      state.value += 1
+    },
+    updateTodo: state => {
+      state.value += 2
+    },
+    deleteTodo: state => {
+      state.value -= 1
+    },
+    updateFilterStatus: state => {
+      state.value += 1
+    }
   }
 });
 
