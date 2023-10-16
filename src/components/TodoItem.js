f9b9062f948f9ec36031402e2285139f6743c1f7
WASHSWATDAVID <39440825+WASHSWATDAVID@users.noreply.github.com>
2023-10-16 16:13:32 +0900

    Get rid of prettier option

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,52 +1,6 @@
-import { format } from 'date-fns';
-import { motion } from 'framer-motion';
-import toast from 'react-hot-toast';
 import React, { useEffect, useState } from 'react';
-import { MdDelete, MdEdit } from 'react-icons/md';
-import { useDispatch } from 'react-redux';
-import { deleteTodo, updateTodo } from '../slices/todoSlice';
-import styles from '../styles/modules/todoItem.module.scss';
-import { getClasses } from '../utils/getClasses';
-import CheckButton from './CheckButton';
-import TodoModal from './TodoModal';
-
-const child = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
 
 function TodoItem({ todo }) {
-  const dispatch = useDispatch();
-  const [checked, setChecked] = useState(false);
-  const [updateModalOpen, setUpdateModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (todo.status === 'complete') {
-      setChecked(true);
-    } else {
-      setChecked(false);
-    }
-  }, [todo.status]);
-
-  const handleCheck = () => {
-    setChecked(!checked);
-    dispatch(
-      updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete' })
-    );
-  };
-
-  const handleDelete = () => {
-    dispatch(deleteTodo(todo.id));
-    toast.success('Todo Deleted Successfully');
-  };
-
-  const handleUpdate = () => {
-    setUpdateModalOpen(true);
-  };
-
   return (
     <>
       <div>Item</div>
